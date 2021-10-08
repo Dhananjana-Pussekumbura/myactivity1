60375f72c634d87226b68191b7a9cb671e449ff9
Dhananjana-Pussekumbura <92115007+Dhananjana-Pussekumbura@users.noreply.github.com>
2021-10-08 20:58:26 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,12 @@
+employee_list = ['Sajana', 'Dhanu', 'Keshan', 'Wanindu', 'Chamika', 'Charith']
+i = 0
+
+while True:
+    print(my_list[i])
+    if (my_list[i] == 'Wanindu'):
+        print('Found the name Wanindu')
+        break
+        print('After break statement')
+    i += 1
+
+print('After while-loop exit')
